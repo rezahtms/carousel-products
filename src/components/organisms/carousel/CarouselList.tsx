bcb9a9b5f46a4783260f8c0006d8cdeeb234ed8d
rezahatampoor <rezahtms@gmail.com>
2024-09-15 17:30:03 +0330

    feat:use spinner in carousel list

--- src/components/organisms/carousel/CarouselList.tsx
+++ src/components/organisms/carousel/CarouselList.tsx
@@ -1,17 +1,16 @@
 import cx from 'classnames';
-import { CarouselItem } from './CarouselItem';
+import { CarouselItem, Spinner } from '@/components';
 import { useGetProducts } from '@/features';
 
 
 export const CarouselList = () => {
-    const {isLoading,products} =useGetProducts();
-    console.log(isLoading,products)
+    const { isLoading, products } = useGetProducts();
+   if(isLoading) return <Spinner/>
     return (
         <ul className={cx('carousel-list flex ', 'w-full')}>
-            {productss?.map(product => (
-            <CarouselItem key={product.id} {...product}/>
-            ))}
+            {products ? products?.map(product => (
+                <CarouselItem key={product.id} {...product} />
+            )) : <p>there are not any product</p>}
         </ul>
     );
 };
-const productss = [{ title: 'hello', id: 1 }, { title: 'hello', id: 2 }]
