1667e65c8813dcf0f329a8ecdccb3aa84964e796
rezahatampoor <rezahtms@gmail.com>
2024-09-15 20:22:12 +0330

    feat:creating link for every product

---
+++ src/components/atoms/ViewProductDetailLink.tsx
@@ -0,0 +1,16 @@
+import cx from 'classnames';
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+interface ViewLinkProps {
+    href: string
+}
+export const ViewProductDetailLink: FC<ViewLinkProps> = ({ href }) => {
+    return (
+        <Link to={`product/${href}`}
+            className={cx('visit-product-details',
+                ' block py-3 bg-black opacity-[0.7] w-full absolute bottom-10 z-50 left-1/2 right-1/2 transform -translate-x-1/2 cursor-pointer')}>
+            <span className={cx('text-center text-[12px] text-white font-bold block')}>View Product {href}</span>
+        </Link>
+
+    )
+}
