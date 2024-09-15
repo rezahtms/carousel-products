1860e6c42999dcb82220ec1346a3652ac9bc5b3e
rezahatampoor <rezahtms@gmail.com>
2024-09-15 16:48:57 +0330

    feat:define product types

---
+++ src/types/product.interface.ts
@@ -0,0 +1,13 @@
+export interface Product {
+    id: string
+    type: string,
+    title: string
+    image: string
+    price: string
+    city: string
+    presenter: string
+    runtime: string
+    rate: string
+    url: string
+
+}
