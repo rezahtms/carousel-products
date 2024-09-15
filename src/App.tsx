1c90fd796dbeb80d4c8157246c519de3b87b77df
rezahatampoor <rezahtms@gmail.com>
2024-09-15 15:13:31 +0330

    feat:create routers app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { HomePage, NotFoundPage, ProductPage } from '@/components';
+
 
 function App() {
 
   return (
-  <div>carousel product</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Outlet/>}>
+      <Route index element={<HomePage/>}/>
+      <Route path='/product/:id' element={<ProductPage/>}/>
+      <Route path='*' element={<NotFoundPage/>}/>
+
+      </Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/pages/index.ts
@@ -0,0 +1,3 @@
+export * from './HomePage';
+export * from './ProductPage';
+export * from './NotFoundPage';
