ad98cd86fe59874b0a07a5acd20023bf3f33fa56
rezahatampoor <rezahtms@gmail.com>
2024-09-15 20:23:20 +0330

    feat:complete carousel Item (It's time to rest )😉

--- src/components/organisms/carousel/CarouselItem.tsx
+++ src/components/organisms/carousel/CarouselItem.tsx
@@ -1,22 +1,24 @@
-import { FC } from "react";
-import cx from 'classnames';
-interface CarouselItemProps {
-    id: string;
-    image: string;
-    title: string;
-}
+import { ViewProductDetailLink } from "../../atoms";  
+import { FC } from "react";  
 
-export const CarouselItem: FC<CarouselItemProps> = ({ image, title }) => {
-    return (
-        <li className={cx('carousel-item', 'flex-shrink-0 w-full h-[70vh] relative')}>
-            <div className={cx('carousel-image', 'absolute inset-0 flex justify-center items-center')}>
-                <img
-                    className="w-full h-full rounded-lg"
-                    src={image}
-                    alt={`${title}-image`}
-                    width="460" height="345"
-                />
-            </div>
-        </li>
-    );
+interface CarouselItemProps {  
+    id: string;  
+    image: string;  
+    title: string;  
+}  
+
+export const CarouselItem: FC<CarouselItemProps> = ({ id, image, title }) => {  
+    return (  
+        <li className="flex-shrink-0 w-full h-[70vh] relative">  
+            <div className="absolute inset-0 flex justify-center items-center">  
+                <img  
+                    className="w-full h-full rounded-lg"  
+                    src={image}  
+                    alt={`${title}-image`}  
+                    width="460" height="345"  
+                />  
+                <ViewProductDetailLink href={id} />  
+            </div>  
+        </li>  
+    );  
 }
--- src/components/organisms/carousel/CarouselList.tsx
+++ src/components/organisms/carousel/CarouselList.tsx
@@ -1,13 +1,16 @@
 import cx from 'classnames';  
 import { useGetProducts } from '@/features';  
-import { Spinner,CarouselItem } from '@/components';  
+import { useStore } from '@/stores';
+import { CarouselItem, Spinner } from '@/components';
+
 
 export const CarouselList = () => {  
     const { products, isLoading } = useGetProducts();  
+    const carouselInd = useStore(state => state.carouselInd);  
     if (isLoading) return <Spinner />;  
     return (  
-        <ul className={cx('carousel-list flex transition-transform duration-1000 ease-in-out', 'w-full')}   
-            style={{ transform: `translateX(-${100}%)` }}>  
+        <ul className={cx('carousel-list flex transition-transform duration-500 ease-in-out', 'w-full')}   
+            style={{ transform: `translateX(-${carouselInd * 100}%)` }}>  
             {products?.map(product => (  
                 <CarouselItem key={product.id} {...product} />  
             ))}  
