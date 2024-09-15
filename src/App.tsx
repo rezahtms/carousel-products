fc834c5aba87ff13f5393b4b52f528c9fda00d0b
rezahatampoor <rezahtms@gmail.com>
2024-09-15 15:17:49 +0330

    style: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,16 @@ import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { HomePage, NotFoundPage, ProductPage } from '@/components';
 
-
 function App() {
-
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Outlet/>}>
-      <Route index element={<HomePage/>}/>
-      <Route path='/product/:id' element={<ProductPage/>}/>
-      <Route path='*' element={<NotFoundPage/>}/>
-
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Outlet />}>
+          <Route index element={<HomePage />} />
+          <Route path='/product/:id' element={<ProductPage />} />
+          <Route path='*' element={<NotFoundPage />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   )
 }
