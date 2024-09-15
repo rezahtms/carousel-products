806d30c17e2ec61d4d81879adcad33bc4f4c6a07
rezahatampoor <rezahtms@gmail.com>
2024-09-15 16:36:41 +0330

    feat:creating catch

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,29 @@
 import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { HomePage, NotFoundPage, ProductPage } from '@/components';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0
+    }
+  }
+});
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Outlet />}>
-          <Route index element={<HomePage />} />
-          <Route path='/product/:id' element={<ProductPage />} />
-          <Route path='*' element={<NotFoundPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Outlet />}>
+            <Route index element={<HomePage />} />
+            <Route path='/product/:id' element={<ProductPage />} />
+            <Route path='*' element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
