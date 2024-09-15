1dd84adc0a3fdfb5d76e836a5db9f3d03c1b9bec
rezahatampoor <rezahtms@gmail.com>
2024-09-15 15:49:16 +0330

    feat:create a route for the back button

--- src/components/pages/NotFoundPage.tsx
+++ src/components/pages/NotFoundPage.tsx
@@ -1,9 +1,9 @@
 import cx from 'classnames';
-import {  useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { BackBtn } from '@/components';
 
 export const NotFoundPage = () => {
-    const navigate=useNavigate();
+    const navigate = useNavigate();
     return (
         <section className={cx('not-found-wrapper',
             "flex items-center justify-center  h-screen")}>
@@ -12,7 +12,7 @@ export const NotFoundPage = () => {
                     <h1 className={cx('not-found-error', 'mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500')}>404</h1>
                     <p className={cx('not-found-message', "mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white")}>Something's missing.</p>
                     <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">Sorry, we can't find that page. You'll find lots to explore on the home page. </p>
-                    <BackBtn title='Back to Homepage' onClick={()=>navigate(-1)} />
+                    <BackBtn title='Back to Homepage' onClick={() => navigate(-1)} />
                 </div>
             </div>
         </section>
