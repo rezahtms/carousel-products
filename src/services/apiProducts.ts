1e8fc36774b3cfe21d6fd3155a4ee8b83264903f
rezahatampoor <rezahtms@gmail.com>
2024-09-15 16:59:07 +0330

    feat:fetch data from api

---
+++ src/services/apiProducts.ts
@@ -0,0 +1,22 @@
+import { Product } from "@/types";
+
+const API_URL = `https://66e20997c831c8811b57050e.mockapi.io/api/v1/home/items`;
+
+export const getProducts = async (): Promise<Product[]> => {
+    try {
+        const response = await fetch(API_URL);
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status} - ${response.statusText}`);
+        }
+        const data = await response.json() as Product[];
+        return data;
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error(`Failed to fetch products: ${error.message}`);
+            throw new Error(`Failed to fetch products:${error.message} `);
+        } else {
+            console.error(`Unexpected error: ${error}`);
+            throw new Error(`Unexpected error occurred ${error}`);
+        }
+    }
+};
