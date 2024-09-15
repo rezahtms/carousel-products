307e639ddab315d443e71c97551f3b3a0feb87ff
rezahatampoor <rezahtms@gmail.com>
2024-09-15 18:53:26 +0330

    style:change some style

--- src/components/organisms/carousel/Carousel.tsx
+++ src/components/organisms/carousel/Carousel.tsx
@@ -4,7 +4,7 @@ import { CarouselBtn, CarouselList, Dots, SvgArrowLeft, SvgArrowRight } from '@/
 export const Carousel = () => {
     return (
         <div
-            className={cx('carousel', 'w-[70vw] h-[70vh] relative border rounded-md  overflow-hidden ')}>
+            className={cx('carousel', 'w-[70vw] h-[70vh] relative border rounded-md overflow-hidden ')}>
             <CarouselList />
             <CarouselBtn title={<SvgArrowLeft />} className='rounded-tr-md rounded-br-md' onClick={() => console.log('prev')} />
             <CarouselBtn title={<SvgArrowRight />} className='right-0 rounded-tl-md rounded-bl-md' onClick={() => console.log('next')} />
