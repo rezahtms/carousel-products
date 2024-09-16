65b84ac701471777e0fc2f80f1118f4eca148260
rezahatampoor <rezahtms@gmail.com>
2024-09-16 03:30:20 +0330

    fix:fix carousel issue

--- src/components/organisms/Dots.tsx
+++ src/components/organisms/Dots.tsx
@@ -5,7 +5,7 @@ export const Dots = () => {
     return (
         <div className={cx('control-dots',
             'flex items-center gap-[8px]  w-fit px-[10px] absolute bottom-3 left-1/2 right-1/2 transform -translate-x-1/2 z-100')}>
-            {Array.from({ length: 15 }, (_, i) => <Dot key={uuidv4()} ind={i+1} />)}
+            {Array.from({ length: 15 }, (_, i) => <Dot key={uuidv4()} ind={i} />)}
         </div>
     );
 }
--- src/stores/global-slice.ts
+++ src/stores/global-slice.ts
@@ -12,13 +12,13 @@ type GlobalActions = {
 
 export type GlobalSlice = GlobalStates & GlobalActions;
 
-export const createGlobalSlice: StateCreator<GlobalSlice, [['zustand/immer', never]], [], GlobalSlice> = (set) => ({
-    carouselInd: 1,
-    setCarouselInd: (ind) => set(state => { state.carouselInd = ind }),
-    nextItem: () => set(state => {
-        state.carouselInd = state.carouselInd <= 15 ? state.carouselInd + 1 : 1;
-    }),
-    prevItem: () => set(state => {
-        state.carouselInd = state.carouselInd >= 1 ? state.carouselInd - 1 : 15;
-    }),
-});
+export const createGlobalSlice: StateCreator<GlobalSlice, [['zustand/immer', never]], [], GlobalSlice> = (set) => ({  
+    carouselInd: 0, // تغییر مقدار به 0  
+    setCarouselInd: (ind) => set(state => { state.carouselInd = ind }),  
+    nextItem: () => set(state => {  
+        state.carouselInd = state.carouselInd < 14 ? state.carouselInd + 1 : 0;  
+    }),  
+    prevItem: () => set(state => {  
+        state.carouselInd = state.carouselInd > 0 ? state.carouselInd - 1 : 14; 
+    }),  
+});  
