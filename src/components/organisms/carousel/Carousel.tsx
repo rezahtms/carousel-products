a3b0e22a4a594ff8673f7af884dff885ffc64fb5
rezahatampoor <rezahtms@gmail.com>
2024-09-16 23:49:03 +0330

    fix: optimize performance by adding useCallback

--- src/components/organisms/carousel/Carousel.tsx
+++ src/components/organisms/carousel/Carousel.tsx
@@ -1,16 +1,19 @@
 import cx from 'classnames';
 import { CarouselBtn, CarouselList, Dots, SvgArrowLeft, SvgArrowRight } from '@/components';
 import { useStore } from '@/stores';
+import { useCallback } from 'react';
 
 export const Carousel = () => {
     const prevItem = useStore(state => state.prevItem);
     const nextItem = useStore(state => state.nextItem);
+    const handlePrevItem = useCallback(() => { prevItem() }, [prevItem])
+    const handleNextItem = useCallback(() => { nextItem() }, [nextItem])
     return (
         <div
             className={cx('carousel', 'w-[70vw] h-[70vh] relative border rounded-md overflow-hidden ')}>
             <CarouselList />
-            <CarouselBtn title={<SvgArrowLeft />} className='rounded-tr-md rounded-br-md' onClick={() => prevItem()} />
-            <CarouselBtn title={<SvgArrowRight />} className='right-0 rounded-tl-md rounded-bl-md' onClick={() => nextItem()} />
+            <CarouselBtn title={<SvgArrowLeft />} className='rounded-tr-md rounded-br-md' onClick={handlePrevItem} />
+            <CarouselBtn title={<SvgArrowRight />} className='right-0 rounded-tl-md rounded-bl-md' onClick={handleNextItem} />
             <Dots />
         </div>
     )
